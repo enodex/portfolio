24eea5907aa82a0582c996134e983aee2c20be6c
Ex Node <107695211+enodex@users.noreply.github.com>
2023-02-07 00:39:49 +0700

    Update Intro.tsx

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -10,33 +10,11 @@ const Intro: FC = () => {
     <div className="relative h-screen flex justify-center items-center flex-col gap-5">
       <Canvas />
 
-      <svg
-        className="h-[10vw] max-h-[100px] min-h-[60px] max-w-[100vw] z-[1]"
-        viewBox="0 0 276 97"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        {characters.map((character, index) => (
-          <Fragment key={character}>
-            <m.path
-              initial={{ pathLength: 0 }}
-              animate={{ pathLength: 1 }}
-              transition={{ delay: index / 10, duration: 1 }}
-              d="Enodex"
-              fill="none"
-              stroke="#FFF"
-              strokeWidth="3"
-            ></m.path>
-            <m.path
-              initial={{ fill: "#ffffff00" }}
-              animate={{ fill: "#ffffff" }}
-              transition={{ delay: 0.7 + index / 10, duration: 0.6 }}
-              fill="none"
-              d="Enodex"
-            ></m.path>
-          </Fragment>
-        ))}
-      </svg>
+     <Typewriter loop
+                ><span>Technologist.</span><span>Designer.</span><span
+                  >Coder.</span
+                ></Typewriter
+              >
       <m.p
         data-scroll
         data-scroll-speed="1"
