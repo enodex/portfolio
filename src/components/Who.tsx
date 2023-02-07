ea4c34583a24fb9da1deab9205476b7bf5f90659
Ex Node <107695211+enodex@users.noreply.github.com>
2023-02-08 02:04:34 +0700

    Update Who.tsx

--- src/components/Who.tsx
+++ src/components/Who.tsx
@@ -21,7 +21,8 @@ const Who: FC = () => {
         {/* Hide my age :v */}
         <p data-scroll className="text-lg text-gray-200" id="story">
           I learning basic programming for almost 3 years, and I started become node validator 1 year ago,
-          I have gained experience in that time and still learning to improve service. I started with testnet chain on several project then decided to expanding and validating mainnet, stake on enodex <10% fee.
+          I have gained experience in that time and still learning to improve service. I started with testnet chain
+          on several project then decided to expanding and validating mainnet, stake on enodex with low fee.
         </p>
       </div>
     </div>
