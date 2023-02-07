908b1480a0a36c9ea0ad1f73640f88eedef12d88
Ex Node <107695211+enodex@users.noreply.github.com>
2023-02-08 00:27:27 +0700

    Update Skills.tsx

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -14,7 +14,7 @@ const Skills: FC<SkillsProps> = ({ skills }) => {
         data-scroll-speed="-1"
         className="text-center text-3xl lg:text-4xl"
       >
-        My skills & Knowledge
+        Enodex Service
       </h1>
       <p
         data-scroll
@@ -22,26 +22,14 @@ const Skills: FC<SkillsProps> = ({ skills }) => {
         data-scroll-speed="1"
         className="description text-lg text-center my-[30px]"
       >
-        Technologies and languages that I use to make my product everyday
+Enodex fully contribute in network and community.
       </p>
-      <div className="grid justify-center grid-cols-4 md:grid-cols-6 gap-y-[10px] md:gap-y-[20px]">
-        {skills.map((skill) => (
-          <a
-            key={skill.link}
-            href={skill.link}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="item flex flex-col items-center justify-around group"
-          >
-            <img
-              className="w-[60px] h-[60px] transition duration-300 group-hover:scale-110"
-              src={skill.image}
-              alt=""
-            />
-            <p className="text-center">{skill.title}</p>
-          </a>
-        ))}
-      </div>
+      
+      <span>Active and monitoring</span>
+      <span>Provide tools</span>
+      <span>Endpoints</span>
+      <span>IBC Relayers</span>
+      <span>Inter-chain Explorer</span>
     </div>
   );
 };
