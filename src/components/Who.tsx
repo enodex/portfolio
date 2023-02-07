ec5a726eb606a46bb6c3813cac49b7780375edf1
Ex Node <107695211+enodex@users.noreply.github.com>
2023-02-08 00:11:36 +0700

    Update Who.tsx

--- src/components/Who.tsx
+++ src/components/Who.tsx
@@ -20,13 +20,8 @@ const Who: FC = () => {
 
         {/* Hide my age :v */}
         <p data-scroll className="text-lg text-gray-200" id="story">
-          My name is Nguyen Anh Phong. I&apos;m a
-          {false ? ` ${new Date().getFullYear() - 2007} years old` : ""}{" "}
-          frontend developer living in Hanoi, Vietnam. I started learning web
-          development when I was 13. Since then, I have made a lot of projects,
-          from basic HTML pages to complex projects like a React library. I hope
-          to be a great developer and get my dream job in the future. Besides
-          coding, I also like{" "}
+          I learning basic programming for almost 3 years, and I started become node validator 1 year ago,
+          I have gained experience in that time and still learning to improve service{" "}
           <a
             className="underline underline-offset-2"
             href="https://blog.napthedev.com/"
