f6b11e22f3762658ac342215e8ac69329f12a505
Ex Node <107695211+enodex@users.noreply.github.com>
2023-02-08 23:03:30 +0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,7 +36,55 @@ const Home: NextPage<HomeProps> = ({ data }) => {
         <Intro />
         <Who />
         <Skills skills={data.skills} />
-        <MainProjects projects={data.projects} />
+        <div data-scroll data-scroll-speed="3" className="lg:flex-1">
+            <div className="border-[#888] border-2 rounded-[20px] overflow-hidden">
+              <div className="border-black border-[8px]">
+                <img
+                  className="w-full h-auto rounded-[12px]"
+                  src="https://raw.githubusercontent.com/enodex/explorer/master/public/favicon.ico" 
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+          <div className="item-info lg:flex-1">
+            <h1 className="text-3xl">{project.title}</h1>
+            <p className="text-[20px] text-justify my-3">
+              "YOYO"
+            </p>
+
+            <div className="flex gap-[5px]">
+              {project.technologies.map((tech) => (
+                <img
+                  key={tech}
+                  className="w-[30px] h-[30px]"
+                  src={tech}
+                  alt=""
+                />
+              ))}
+            </div>
+
+            <div className="flex mt-[25px] gap-[20px]">
+              <a
+                href={project.demo}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="h-[45px] px-[15px] text-white rounded transition duration-300 flex items-center gap-[10px] bg-[#1876d2] hover:bg-[#2884e0]"
+              >
+                <BiLinkExternal size={25} />
+                <span> Live Demo</span>
+              </a>
+              <a
+                href={project.github}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="h-[45px] px-[15px] text-white rounded transition duration-300 flex items-center gap-[10px] bg-[#1b222b] hover:bg-[#191e25]"
+              >
+                <FaGithub size={25} />
+                <span> View Github</span>
+              </a>
+            </div>
+          </div>
         <SmallProjects projects={data.smallProjects} />
         <Contact />
       </div>
